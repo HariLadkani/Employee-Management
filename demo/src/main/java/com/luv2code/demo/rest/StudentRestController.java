b85107f0b20c6fb6b522b254a7a5f541473d4204
Hari Lal <hl836@drexel.edu>
2024-09-07 15:00:29 -0400

    "Built getStudent endpoint:REST API"

---
+++ demo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,39 @@
+package com.luv2code.demo.rest;
+
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    //post construct
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Hari", "Lal"));
+        theStudents.add(new Student("Savi", "Kumari"));
+        theStudents.add(new Student("Daines", "John"));
+
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
+    }
+}
