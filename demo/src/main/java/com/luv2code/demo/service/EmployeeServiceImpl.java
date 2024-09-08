9b53320c5fcb6cd426da46f23e48d22dd1d0706b
Hari Lal <hl836@drexel.edu>
2024-09-08 13:16:04 -0400

    Add CRUD operations to EmployeeService(Service Layer for REST)
    - Implemented `findById(int theId)` method to find an employee by ID.
    - Implemented `save(Employee theEmployee)` method to save or update an employee.
    - Implemented `deleteById(int theId)` method to delete an employee by ID.
    - Utilized EmployeeDAO for database interactions with JPA

--- demo/src/main/java/com/luv2code/demo/service/EmployeeServiceImpl.java
+++ demo/src/main/java/com/luv2code/demo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.luv2code.demo.dao.EmployeeDAO;
 import com.luv2code.demo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,5 +21,25 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = employeeDAO.save(theEmployee); // merge: id==0, insert else update
+        return dbEmployee;
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = findById(theId);
+        employeeDAO.deleteById(theId);
+
     }
 }
